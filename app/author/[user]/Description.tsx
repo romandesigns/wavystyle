d99045f6c2deac7bb584ec19d1fbbf5f54095dfb
Roman F. <wavystyledev@gmail.com>
2024-08-11 22:00:23 -0400

    added underline text

--- app/author/[user]/Description.tsx
+++ app/author/[user]/Description.tsx
@@ -6,16 +6,22 @@ const CopyWriting = () => {
     <article className="flex flex-col items-start sm:bg-[#070816] p-6 rounded-lg relative overflow-hidden order-2 md:order-1">
       <div className="z-[1] w-full">
         <Disciplines />
-        <h1 className="text-4xl sm:text-4xl text-white font-black relative animate-gradient-border max-w-2xl">
+        <h1
+          className="text-4xl sm:text-3xl text-white font-black relative animate-gradient-border max-w-3xl"
+          style={{ lineHeight: 1.4 }}
+        >
           Experienced{" "}
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-brand-warm to-brand-cool animate-gradient-text bg-[length:200%_200%]">
             Full Stack Developer
           </span>{" "}
-          with a Passion for User Experience and UI Design.
+          with a passion for creating seamless{" "}
+          <span className="underline-wavy">user experiences</span> and{" "}
+          <span className="underline-wavy">intuitive UI</span> design.
         </h1>
         <p className="text-neutral-400 italic  my-10 text-[0.8rem] max-w-lg  leading-relaxed">
-          Over 8 years of experience developing feature-rich applications for
-          various international and well-established companies.
+          I&apos;m always on the lookout for exciting projects and
+          collaborations. Whether you&apos;re a like-minded enthusiast or a
+          company seeking top-notch development skills, feel free to reach out!
         </p>
         <button className="rounded-md bg-brand-cool w-full sm:w-2/6">
           <a
